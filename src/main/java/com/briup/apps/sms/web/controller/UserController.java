002f7c722fe38aaa12e14e870683e31296d501e7
李春雨 <licy@briup.com>
2019-07-09 10:57:50 +0800

    [李春雨]为用户控制器添加了注释

--- src/main/java/com/briup/apps/sms/web/controller/UserController.java
+++ src/main/java/com/briup/apps/sms/web/controller/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 用户控制
+ * */
 @RestController
 @RequestMapping("/user")
 public class UserController {
