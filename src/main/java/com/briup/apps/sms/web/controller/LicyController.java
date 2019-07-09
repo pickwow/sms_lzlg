728bdac53eaedb2cb714eab07d66f2a68fac7eb9
李春雨 <licy@briup.com>
2019-07-09 14:45:38 +0800

    [licy]添加了两行代码

--- src/main/java/com/briup/apps/sms/web/controller/LicyController.java
+++ src/main/java/com/briup/apps/sms/web/controller/LicyController.java
@@ -1,5 +1,10 @@
 package com.briup.apps.sms.web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/licy")
 public class LicyController {
 
 }
